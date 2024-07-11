6b0e8a37bfd85580ab1322f7f1335a83dbadf7c7
csavelief <csavelief@mncc.fr>
2024-07-11 16:12:15 +0200

    Play with Botman's fallback.

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -4,27 +4,39 @@ namespace App\Http\Controllers;
 
 use BotMan\BotMan\BotMan;
 use BotMan\BotMan\Messages\Incoming\Answer;
+use Illuminate\Support\Str;
 
 class BotManController extends Controller
 {
     public function handle(): void
     {
         $botman = app('botman');
-        $botman->hears('{message}', function (BotMan $botman, string $message) {
-            if ($message == 'hi') {
-                $this->askName($botman);
-            } else {
-                $botman->reply("write 'hi' for testing...");
+        $botman->hears('.*(hi|hello|bonjour).*', function (BotMan $botman, string $message) {
+            if (Str::lower($message) === 'hi' || Str::lower($message) === 'hello') {
+                $this->askNameEn($botman);
+            } else if (Str::lower($message) === 'bonjour') {
+                $this->askNameFr($botman);
             }
+        });
+        $botman->fallback(function (BotMan $botman) {
+            $botman->reply('Sorry, I did not understand these commands.');
         });
         $botman->listen();
     }
 
-    public function askName(BotMan $botman): void
+    private function askNameEn(BotMan $botman): void
     {
-        $botman->ask('Hello! What is your Name?', function (Answer $answer) use ($botman) {
+        $botman->ask('Hello! What is your name?', function (Answer $answer) use ($botman) {
             $name = $answer->getText();
-            $this->say('Nice to meet you ' . $name);
+            $this->say("Nice to meet you {$name}!");
+        });
+    }
+
+    private function askNameFr(BotMan $botman): void
+    {
+        $botman->ask('Bonjour! Quel est ton nom?', function (Answer $answer) use ($botman) {
+            $name = $answer->getText();
+            $this->say("Enchanté, {$name}!");
         });
     }
 }
